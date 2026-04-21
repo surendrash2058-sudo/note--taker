03b6dfb132648ab3d2790aa83cf462bd73d208b4
Hasanul Islam Sujon <hisujon180@gmail.com>
2026-04-21 11:16:21 +0900

    Test GitHub

--- preload.js
+++ preload.js
@@ -1,7 +1,16 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+    // Basic features
     saveNote: (text) => ipcRenderer.invoke('save-note', text),
     loadNote: () => ipcRenderer.invoke('load-note'),
-    deleteNote: () => ipcRenderer.invoke('delete-note')
+    deleteNote: () => ipcRenderer.invoke('delete-note'),
+
+    // New features (from your PDF)
+    saveAs: (text) => ipcRenderer.invoke('save-as', text),
+    openFile: () => ipcRenderer.invoke('open-file'),
+    newNote: () => ipcRenderer.invoke('new-note'),
+
+    // Smart save (fixes Save button behavior)
+    smartSave: (text, filePath) => ipcRenderer.invoke('smart-save', text, filePath)
 });
